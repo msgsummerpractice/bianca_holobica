da3db560cd6490e28c16f464fa2624ab92154201
biancaholobica <bianca.holobica@stud.ubbcluj.ro>
2026-07-23 15:53:11 +0300

    Refactor the same application to use Java-based configuration (@Configuration, @Bean)

--- new-project/src/main/java/com/example/App.java
+++ new-project/src/main/java/com/example/App.java
@@ -1,6 +1,8 @@
 package com.example;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -10,8 +12,14 @@ public class App
 {
     public static void main( String[] args )
     {
+        /* 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Intern intern = context.getBean("intern", Intern.class);
+        intern.display();
+         */
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Intern intern = context.getBean(Intern.class);
         intern.display();
     }
 }
