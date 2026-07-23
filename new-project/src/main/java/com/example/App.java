25201929e5a65d521721a0b4e95869340a52404c
biancaholobica <bianca.holobica@stud.ubbcluj.ro>
2026-07-23 15:33:02 +0300

    Create a simple Spring application using XML configuration

---
+++ new-project/src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/**
+ * Hello world!
+ *
+ */
+ 
+public class App
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Intern intern = context.getBean("intern", Intern.class);
+        intern.display();
+    }
+}
---
+++ new-project/src/main/java/com/example/Intern.java
@@ -0,0 +1,13 @@
+package com.example;
+ 
+import org.springframework.context.annotation.Configuration;
+ 
+@Configuration
+public class Intern {
+    public void display() {
+      System.out.println("Hello Spring");
+    }
+   
+}
+ 
+ 
